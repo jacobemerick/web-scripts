2fc7a4ac888cd6d21e7610bf24b16aceff4c2de8
jacobemerick <jpemeric@gmail.com>
2016-02-08 16:17:35 -0700

    Adds beginning of blog fetching cron

---
+++ cron/fetch-blog-activity.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+$mostRecentBlogDateTime = $db->getRead()->fetchValue(
+    "SELECT `datetime` FROM `jpemeric_stream`.`blog` ORDER BY `datetime` DESC LIMIT 1"
+);
+$mostRecentBlogDateTime = new DateTime($mostRecentBlogDateTime);
+
+$blogFeed = Feed::loadRss('http://blog.jacobemerick.com/rss.xml');
+foreach ($blogFeed->item as $item) {
+    $dateTime = new DateTime($item->pubDate);
+    if ($dateTime <= $mostRecentBlogDateTime) {
+        // break;
+    }
+
+    $uniqueBlogCheck = $db->getRead()->fetchValue(
+        "SELECT 1 FROM `jpemeric_stream`.`blog` WHERE `permalink` = :guid LIMIT 1",
+        ['guid' => (string) $item->guid]
+    );
+    if ($uniqueBlogCheck !== false) {
+        continue;
+    }
+
+    $dateTime->setTimezone(new DateTimeZone('America/Phoenix'));
+    $metadata = json_decode(json_encode($item), true);
+
+    $db->getWrite()->perform(
+        "INSERT INTO `jpemeric_stream`.`blog` (`permalink`, `datetime`, `metadata`) " . 
+        "VALUES (:permalink, :datetime, :metadata)",
+        [
+            'permalink' => (string) $item->guid,
+            'datetime' => $dateTime->format('Y-m-d H:i:s'),
+            'metadata' => json_encode($metadata),
+        ]
+    );
+}
+
+exit('hi');
