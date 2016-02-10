482726d07cfa28514d0e1b3e8072d462e2c693af
jacobemerick <jpemeric@gmail.com>
2016-02-09 21:02:00 -0700

    Adds final raw cron for blog activities

--- cron/fetch-blog-activity.php
+++ cron/fetch-blog-activity.php
@@ -36,4 +36,36 @@ foreach ($blogFeed->item as $item) {
     );
 }
 
-exit('hi');
+$mostRecentBlogCommentDateTime = $db->getRead()->fetchValue(
+    "SELECT `datetime` FROM `jpemeric_stream`.`blog_comment` ORDER BY `datetime` DESC LIMIT 1"
+);
+$mostRecentBlogCommentDateTime = new DateTime($mostRecentBlogCommentDateTime);
+
+$commentFeed = Feed::loadRss('http://blog.jacobemerick.com/rss-comments.xml');
+foreach ($commentFeed->item as $item) {
+    $dateTime = new DateTime($item->pubDate);
+    if ($dateTime <= $mostRecentBlogCommentDateTime) {
+        break;
+    }
+
+    $uniqueBlogCommentCheck = $db->getRead()->fetchValue(
+        "SELECT 1 FROM `jpemeric_stream`.`blog_comment` WHERE `permalink` = :guid LIMIT 1",
+        ['guid' => (string) $item->guid]
+    );
+    if ($uniqueBlogCommentCheck !== false) {
+        continue;
+    }
+
+    $dateTime->setTimezone(new DateTimeZone('America/Phoenix'));
+    $metadata = json_decode(json_encode($item), true);
+
+    $db->getWrite()->perform(
+        "INSERT INTO `jpemeric_stream`.`blog_comment` (`permalink`, `datetime`, `metadata`) " .
+        "VALUES (;permalink, :datetime, :metadata)",
+        [
+            'permalink' => (string) $item->guid,
+            'datetime' => $dateTime->format('Y-m-d H:i:s'),
+            'metadata' => json_encode($metadata),
+        ]
+    );
+}
